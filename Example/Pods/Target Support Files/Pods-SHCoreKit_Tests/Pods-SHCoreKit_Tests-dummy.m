6f75e5b7b6e4beae0af64b95b3e0d1b1b03c59e1
Ray <ray@kumu.ph>
2022-03-17 14:21:26 +0800

    更新

---
+++ Example/Pods/Target Support Files/Pods-SHCoreKit_Tests/Pods-SHCoreKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SHCoreKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_SHCoreKit_Tests
+@end
---
+++ Example/Pods/Target Support Files/SHCoreKit/SHCoreKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SHCoreKit : NSObject
+@end
+@implementation PodsDummy_SHCoreKit
+@end
